a36b91f4387548c6005ef0b786f9f214507623f1
Tanwinder <m_702677@macltus42973.attlocal.net>
2020-04-21 15:55:49 -0400

    Updates

---
+++ precache-manifest.90f89066e176d3d5e2af175c96093f5d.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcb6e371e6ac1062df60eb5bcf9a27f3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a86391200e0d0fc2b23c",
+    "url": "/static/css/main.3563a9ca.chunk.css"
+  },
+  {
+    "revision": "a2800aa2cb615d640d27",
+    "url": "/static/js/2.5a02252a.chunk.js"
+  },
+  {
+    "revision": "702e06c21f4db63e7e05a8a0eda9baf1",
+    "url": "/static/js/2.5a02252a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a86391200e0d0fc2b23c",
+    "url": "/static/js/main.2978603f.chunk.js"
+  },
+  {
+    "revision": "b663a1e7e3d8ccf6ccaf",
+    "url": "/static/js/runtime-main.b926948d.js"
+  },
+  {
+    "revision": "7810adda9143962d61d640c239f10fff",
+    "url": "/static/media/Smart-Home-Door-Lock.7810adda.jpg"
+  },
+  {
+    "revision": "3eb1fef12235ddeafb7651ee2b6116b1",
+    "url": "/static/media/about-symbol1.3eb1fef1.png"
+  },
+  {
+    "revision": "e454db4a29587d13eb6408f2116653e4",
+    "url": "/static/media/about.e454db4a.jpg"
+  },
+  {
+    "revision": "62ef849bb1ff6acc778a68c35086cc65",
+    "url": "/static/media/commercial.62ef849b.jpg"
+  },
+  {
+    "revision": "7cf020404b847fa29c11645f0b595879",
+    "url": "/static/media/door-alarm.7cf02040.jpg"
+  },
+  {
+    "revision": "69aa5b2c6a38b043d84cee5e0d928b6f",
+    "url": "/static/media/fire-alarms.69aa5b2c.jpg"
+  },
+  {
+    "revision": "92e762846fd7865bdd5e118207bfbbb0",
+    "url": "/static/media/garage-door.92e76284.jpg"
+  },
+  {
+    "revision": "97235daae2bf768fe43264008be0f62e",
+    "url": "/static/media/history.97235daa.png"
+  },
+  {
+    "revision": "b48d081923c2d7ccdba01dac1c6a83ce",
+    "url": "/static/media/home-screen-image.b48d0819.jpg"
+  },
+  {
+    "revision": "15c21e793740a1001ec33dfef8a618e0",
+    "url": "/static/media/homeAutomation4.15c21e79.jpg"
+  },
+  {
+    "revision": "c66329fd60ad03961d438243de08087b",
+    "url": "/static/media/logo11.c66329fd.png"
+  },
+  {
+    "revision": "01cef9dcfaa0c318473d618571aaf8e4",
+    "url": "/static/media/residential.01cef9dc.jpg"
+  },
+  {
+    "revision": "88a001e439a28078a17bc4f2593d07d1",
+    "url": "/static/media/security-cameras.88a001e4.jpg"
+  },
+  {
+    "revision": "63efd569ddd619657145a1e019705918",
+    "url": "/static/media/specialize.63efd569.png"
+  },
+  {
+    "revision": "ac953f490f4c27173c7f03039daeb6ce",
+    "url": "/static/media/today.ac953f49.png"
+  }
+]);
