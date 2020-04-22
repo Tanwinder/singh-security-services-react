dc92d18ed9a57235354adf452012937dbfc25058
Tanwinder <m_702677@macltus42973.attlocal.net>
2020-04-22 18:34:11 -0400

    update home page

--- src/data/homepagedata.js
+++ src/data/homepagedata.js
@@ -1,4 +1,9 @@
 import homeAutomation4 from '../assetfiles/images/homeAutomation4.jpg';
+import smartHome from '../assetfiles/images/smartHome.jpg';
+import cctvcamera1 from '../assetfiles/images/cctvcamera1.jpg';
+import burglaryAlarmSystems from '../assetfiles/images/burglaryAlarmSystems.jpg';
+import smokeDetector from '../assetfiles/images/smokeDetector.jpg';
+import structuredCabling from '../assetfiles/images/structuredCabling.png';
 export const mainheading = {
     classvalue: 'fullwidth full',
     heading: 'Singh Security Services',
@@ -9,14 +14,14 @@ export const imagecontent = [
     {
       header: 'Home Security',
     //   imagesrc: 'http://securitycompanies.co/wp-content/uploads/2017/08/Best-and-Most-Affordable-Home-Security-System-in-2017.jpg',
-      imagesrc: homeAutomation4,
+      imagesrc: smartHome,
       paragraph: 'Protect your home and family with our advanced home security monitoring systems.',
       imageclass: 'image-double'
     },
     {
       header: 'Security Cameras',
       // imagesrc: 'https://i.ytimg.com/vi/nlHBaO7I9tw/maxresdefault.jpg',
-      imagesrc: homeAutomation4,
+      imagesrc: cctvcamera1,
       paragraph: 'Whether you are shopping, on vacation or at work, high definition security cameras let you view and hear live video streaming on your computer or mobile device.',
       imageclass: 'image-double'
     },
@@ -29,21 +34,21 @@ export const imagecontent = [
     {
       header: 'Burglary Alarm Systems',
       // imagesrc: 'https://mce-engineering.com/wp-content/uploads/2017/02/NEC-VoIP-Phone-Systems1.jpg',
-      imagesrc: homeAutomation4,
+      imagesrc: burglaryAlarmSystems,
       paragraph: 'Interactive Alarm Systems with remote mobile access and full notifications',
       imageclass: 'image-double'
     },
     {
       header: 'Fire, Smoke & Heat',
       // imagesrc: 'https://www.safetysolutions.in/images/smoke.jpg',
-      imagesrc: homeAutomation4,
+      imagesrc: smokeDetector,
       paragraph: 'When every second counts, sensors throughout your home instantly alert our monitoring centers in the event of a fire or gas leak.',
       imageclass: 'image-double last'
     },
     {
       header: 'Structured Cabling',
       // imagesrc: 'https://www.pci-fla.com/technology-services/structured-cabling/structured-cabling.png',
-      imagesrc: homeAutomation4,
+      imagesrc: structuredCabling,
       paragraph: 'Structured Cabling is the core element of the corporate network',
       imageclass: 'image-double'
     },
